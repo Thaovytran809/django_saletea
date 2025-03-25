11c30b960c004625661063deefb78a19365ac475
Thaovytran809 <Thaovytran809@gmail.com>
2025-03-26 00:49:53 +0700

    Update settings.py to fix ALLOWED_HOSTS

--- saletea/settings.py
+++ saletea/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u6-9eq@fk4y1-#5$mm11uinkyjj9rjtncnidc1og#p@5p1*t30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["trskhoailang.pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
